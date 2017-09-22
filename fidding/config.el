0d07bfd806704f39f96fc9bd2b1c2f878d9d38e1
fidding <fidding@sunbar.cn>
2017-09-22 09:13:24 +0800

    移除垃圾回收设置，会导致切换进来卡顿

--- fidding/config.el
+++ fidding/config.el
@@ -50,13 +50,13 @@
 (setq ns-use-srgb-colorspace nil)
 
 ;;设置垃圾回收，在Windows下，emacs25版本会频繁出发垃圾回收，所以需要设置
-(when (eq system-type 'windows-nt)
-(setq gc-cons-threshold (* 512 1024 1024))
-(setq gc-cons-percentage 0.5)
-(run-with-idle-timer 5 t #'garbage-collect)
+;;(when (eq system-type 'windows-nt)
+;;(setq gc-cons-threshold (* 512 1024 1024))
+;;(setq gc-cons-percentage 0.5)
+;;(run-with-idle-timer 5 t #'garbage-collect)
 ;;显示垃圾回收信息，这个可以作为调试用
 ;;(setq garbage-collection-messages t)
-)
+;;)
 
 ;;
 ;;更改spacemacs内置设置
